eb76d70e4e2fdaba1addae3326a5c95118569fa2
Julio Salvino <juliosalvino@gmail.com>
2023-11-22 16:56:12 -0300

    banco de dados configurado

--- agenda_Projeto/settings.py
+++ agenda_Projeto/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'agenda_Projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Dio_Django',
-        'USER': 'postgres',
-        'PASSWORD': 'rajkumari123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
